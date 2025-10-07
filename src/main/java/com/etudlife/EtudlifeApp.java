2e790ab97ec9241bb9ad6b4fb6ddf534e8ca3e4d
Lynabch <42008865@parisnanterre.fr>
2025-10-07 18:17:22 +0200

    First Commit - v0 structure projet

---
+++ src/main/java/com/etudlife/EtudlifeApp.java
@@ -0,0 +1,12 @@
+package com.etudlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudlifeApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EtudlifeApp.class, args);
+
+    }
+}
