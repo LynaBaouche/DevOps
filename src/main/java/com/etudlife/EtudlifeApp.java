04eee6e0411573aa262166a52ab77999ef631699
Lynabch <42008865@parisnanterre.fr>
2025-10-08 21:28:50 +0200

    création packages

--- src/main/java/com/etudlife/EtudlifeApp.java
+++ src/main/java/com/etudlife/EtudlifeApp.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EtudlifeApp {
     public static void main(String[] args) {
-        SpringApplication.run(EtudlifeApp.class, args);
+        SpringApplication.run(EtudlifeApp.class, args) ;
+
 
     }
 }
