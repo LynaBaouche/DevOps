c3b611fe3946e4c79939c55f7ff0e96861c62538
Lynabch <42008865@parisnanterre.fr>
2026-01-14 15:25:15 +0100

    test build

--- src/test/java/SocialIntegrationTest.java
+++ src/test/java/SocialIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = EtudlifeApp.class)
