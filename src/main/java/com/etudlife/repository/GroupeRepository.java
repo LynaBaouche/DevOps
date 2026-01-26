7ae4e992d75783c17c84032c1e32658ac690a446
kenza <menadkenza2022@gmail.com>
2026-01-26 19:08:03 +0100

    docker

--- src/main/java/com/etudlife/repository/GroupeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.etudlife.repository;
-
-import com.etudlife.model.Groupe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupeRepository extends JpaRepository<Groupe, Long> {}
